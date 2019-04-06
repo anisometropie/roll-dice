558373ebd67e39656def1e924a85c83dc4e897fa
Valentin Chanas <valentin.chanas@dcbrain.com>
2019-04-06 13:44:19 +0200

    create app

---
+++ dice.js
@@ -0,0 +1,12 @@
+const normal = [
+  { value: 1, proba: 1 / 6 },
+  { value: 2, proba: 1 / 6 },
+  { value: 3, proba: 1 / 6 },
+  { value: 4, proba: 1 / 6 },
+  { value: 5, proba: 1 / 6 },
+  { value: 6, proba: 1 / 6 }
+];
+
+const red = [{ value: 4, proba: 5 / 6 }, { value: 9, proba: 1 / 6 }];
+
+module.exports = { normal, red };
---
+++ main.js
@@ -0,0 +1,22 @@
+const { get } = require("lodash");
+const { red, normal } = require("./dice.js");
+
+const rollDie = die => {
+  const rand = Math.random();
+  let result;
+  die.reduce((acc, cur) => {
+    const currentProba = acc + cur.proba;
+    if (acc <= rand && rand < currentProba) {
+      result = cur.value;
+    }
+    return currentProba;
+  }, 0);
+  return result;
+};
+
+let result = {};
+for (let i = 0; i < 100; i++) {
+  const roll = rollDie(red);
+  result[roll] = get(result, roll, 0) + 1;
+}
+console.log(result);

32a8c7c978c6afe86f90bcbb4af4dcbbea2b3d6a
Valentin Chanas <valentin.chanas@dcbrain.com>
2019-04-06 20:20:11 +0200

    more dice

--- dice.js
+++ dice.js
@@ -5,8 +5,12 @@ const normal = [
   { value: 4, proba: 1 / 6 },
   { value: 5, proba: 1 / 6 },
   { value: 6, proba: 1 / 6 }
-];
+]
 
-const red = [{ value: 4, proba: 5 / 6 }, { value: 9, proba: 1 / 6 }];
+const red = [{ value: 4, proba: 5 / 6 }, { value: 9, proba: 1 / 6 }]
+const blue = [{ value: 2, proba: 1 / 2 }, { value: 7, proba: 1 / 2 }]
+const olive = [{ value: 0, proba: 1 / 6 }, { value: 5, proba: 5 / 6 }]
+const yellow = [{ value: 3, proba: 4 / 6 }, { value: 8, proba: 2 / 6 }]
+const magenta = [{ value: 1, proba: 2 / 6 }, { value: 6, proba: 4 / 6 }]
 
-module.exports = { normal, red };
+module.exports = { normal, red, blue, olive, yellow, magenta }

162b75fcd63fce1dd786043d5f9b584dbd32e74c
Valentin Chanas <valentin.chanas@dcbrain.com>
2019-04-06 13:58:20 +0200

    prettier

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
-const { get } = require("lodash");
-const { red, normal } = require("./dice.js");
-const { gcd } = require("math.js");
+const { get } = require('lodash')
+const { red, normal } = require('./dice.js')
+const { gcd } = require('./math.js')
 
 const rollDie = die => {
-  const rand = Math.random();
-  let result;
+  const rand = Math.random()
+  let result
   die.reduce((acc, cur) => {
-    const currentProba = acc + cur.proba;
+    const currentProba = acc + cur.proba
     if (acc <= rand && rand < currentProba) {
-      result = cur.value;
+      result = cur.value
     }
-    return currentProba;
-  }, 0);
-  return result;
-};
-
-let result = {};
-for (let i = 0; i < 100; i++) {
-  const roll = rollDie(red);
-  result[roll] = get(result, roll, 0) + 1;
+    return currentProba
+  }, 0)
+  return result
 }
-console.log(result);
+
+rollDice = (...dice) => {
+  return dice.reduce((acc, cur) => [...acc, rollDie(cur)], [])
+}
+
+let result = {}
+for (let i = 0; i < 100; i++) {
+  const roll = rollDie(red)
+  result[roll] = get(result, roll, 0) + 1
+}
+console.log(rollDice(red, normal))

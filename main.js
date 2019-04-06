a608a0c4038df8d0080fb97eec044be0a71e0fb1
Valentin Chanas <valentin.chanas@dcbrain.com>
2019-04-06 20:20:22 +0200

    other roll

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-const { get } = require('lodash')
-const { red, normal } = require('./dice.js')
+const util = require('util')
 const { gcd } = require('./math.js')
+const { red, blue, olive, yellow, magenta, normal } = require('./dice.js')
+const { get, isEqual, find, sortBy } = require('lodash')
 
 const rollDie = die => {
   const rand = Math.random()
@@ -19,9 +20,37 @@ rollDice = (...dice) => {
   return dice.reduce((acc, cur) => [...acc, rollDie(cur)], [])
 }
 
-let result = {}
-for (let i = 0; i < 100; i++) {
-  const roll = rollDie(red)
-  result[roll] = get(result, roll, 0) + 1
+let results = []
+for (let i = 0; i < 1000000; i++) {
+  // const roll = rollDice(normal, normal, normal, normal, normal).sort()
+  const roll = rollDice(
+    red,
+    red,
+    blue,
+    blue,
+    olive,
+    olive,
+    yellow,
+    yellow,
+    magenta,
+    magenta
+  ).sort()
+  let result = find(results, result => isEqual(result.roll, roll)) || {
+    roll,
+    count: 0
+  }
+  result.count++
+  if (result.count === 1) {
+    results.push(result)
+  }
 }
-console.log(rollDice(red, normal))
+
+const resultsByRoll = sortBy(results, 'roll')
+const resultsByCount = sortBy(results, 'count')
+
+console.log(util.inspect(resultsByRoll, { maxArrayLength: null }))
+console.log('')
+console.log(util.inspect(resultsByCount, { maxArrayLength: null }))
+console.log(
+  find(results, result => isEqual(result.roll, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
+)
